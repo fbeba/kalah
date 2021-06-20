c0de3d67274b6a137671239eceaaf251bcc20e85
fbeba <filip.beba@gmail.com>
2021-06-20 09:49:53 +0200

    Score moved from Player to Game

--- src/test/java/com/bbt/kalah/service/GameStatusTest.java
+++ src/test/java/com/bbt/kalah/service/GameStatusTest.java
@@ -3,6 +3,11 @@ package com.bbt.kalah.service;
 import com.bbt.kalah.model.GameState;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static com.bbt.kalah.model.GameState.FINISHED;
 import static com.bbt.kalah.model.GameState.ONGOING;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +24,39 @@ public class GameStatusTest extends TestHelper {
     }
 
     @Test
+    public void givenGame_whenAskedForState_currentBoardStateIsReturned() {
+        // given
+        final var stoneQuantities = new int[]{
+                0, 1, 0, 0, 0, 2, 9,
+                0, 0, 1, 5, 0, 0, 20
+        };
+        final var id = setupGameWithCustomBoard(stoneQuantities);
+        gamesService.makeMove(id, 2);
+
+        // when
+        final var boardStatus = gamesService.getGameStatus(id);
+
+        // then
+        assertThat(boardStatus.getStatus())
+                .containsAllEntriesOf(Stream.of(new String[][]{
+                        {"1", "0"},
+                        {"2", "0"},
+                        {"3", "0"},
+                        {"4", "0"},
+                        {"5", "0"},
+                        {"6", "2"},
+                        {"7", "15"},
+                        {"8", "0"},
+                        {"9", "0"},
+                        {"10", "1"},
+                        {"11", "0"},
+                        {"12", "0"},
+                        {"13", "0"},
+                        {"14", "20"}})
+                        .collect(Collectors.toMap(entry -> entry[0], entry -> entry[1])));
+    }
+
+    @Test
     public void ongoingGameDoesntReportScore() {
         // when
         final var id = gamesService.setupNew();
@@ -31,8 +69,8 @@ public class GameStatusTest extends TestHelper {
     public void givenFinishedGame_whenAskedForScore_scoreIsAvailable() {
         // given
         final var stoneQuantities = new int[]{
-                0, 1, 0, 0, 0, 2,   9,
-                0, 0, 0, 5, 0, 0,  20
+                0, 1, 0, 0, 0, 2, 9,
+                0, 0, 0, 5, 0, 0, 20
         };
         final var id = setupGameWithCustomBoard(stoneQuantities);
         gamesService.makeMove(id, 2);
@@ -44,5 +82,6 @@ public class GameStatusTest extends TestHelper {
         // then
         assertThat(score).isEqualTo("17-20");
     }
+
 
 }
